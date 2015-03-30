ca5f6d9024875a233b46cd05d114e61d2af98b89
two.erics <eric+eric>
2015-03-30 12:04:05 -0400

    Continue writing layout of Game classes

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,5 +1,27 @@
 class MineSweeper
 
+
+    #initialize
+      #creates board object or gets saved one
+
+    #run game method
+    #game loop until over?
+      #render board
+      #ask user what they want to do
+      #get coordinates for that
+      #change board state
+      #check win/loss
+      #end
+    # messages about end game state
+
+  def render
+  end
+
+  def display
+    puts render
+  end
+
+
 end
 
 
@@ -14,14 +36,22 @@ class Board
     [-1,0],
     [-1,-1]
   ]
-
+  #array of arrays that = board state
+  #array of bombs created in initialize or passed from prev. game
+  #array of flags
+  #array of revealed tile objects
   #
+
+
+
 end
 
 
 class Tile
-  #neighbors
-  #neighbor bomb count
+  #initialize (parent = nil)
+  #neighbors - array of tile objects
+  #neighbor bomb count - integer
+    #loop through neighbors array, bomb? generates neighbor bomb count
   #bomb?
   #revealed?
   #flagged?
