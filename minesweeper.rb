9c0d42d31c913f95fa01492d7711a229e3b49634
two.erics <eric+eric>
2015-03-30 11:56:18 -0400

    Write layout of Game classes

---
+++ minesweeper.rb
@@ -0,0 +1,29 @@
+class MineSweeper
+
+end
+
+
+class Board
+  NEIGHBORS = [
+    [1,1],
+    [1,-1],
+    [1,0],
+    [0,-1],
+    [0,1],
+    [-1,-1],
+    [-1,0],
+    [-1,-1]
+  ]
+
+  #
+end
+
+
+class Tile
+  #neighbors
+  #neighbor bomb count
+  #bomb?
+  #revealed?
+  #flagged?
+
+end
