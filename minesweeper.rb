55ceb8eb58401fbe114dd1aa8e49458520ae2306
two.erics <eric+eric>
2015-03-30 12:31:23 -0400

    Give Tiles initialize and find_neighbors method

--- minesweeper.rb
+++ minesweeper.rb
@@ -26,6 +26,32 @@ end
 
 
 class Board
+
+  GRID_SIZE = 9
+
+  def initialize()
+  end
+  #array of arrays that = board state
+  #array of bombs created in initialize or passed from prev. game
+  #array of flags
+  #array of revealed tile objects
+  #
+
+  def [](pos)
+    row, col = pos[0], pos[1]
+    @position[row][col]
+  end
+
+  def []=(pos, state)
+
+  end
+
+
+end
+
+
+class Tile
+
   NEIGHBORS = [
     [1,1],
     [1,-1],
@@ -36,24 +62,32 @@ class Board
     [-1,0],
     [-1,-1]
   ]
-  #array of arrays that = board state
-  #array of bombs created in initialize or passed from prev. game
-  #array of flags
-  #array of revealed tile objects
-  #
 
-
-
-end
-
-
-class Tile
   #initialize (parent = nil)
+  def initialize(position, board, parent = nil)
+    @position = position
+    @parent = parent
+    @neighbors = find_neighbors(board)
+    @board = board
+  end
+
   #neighbors - array of tile objects
   #neighbor bomb count - integer
     #loop through neighbors array, bomb? generates neighbor bomb count
   #bomb?
   #revealed?
   #flagged?
+
+  def find_neighbors(board)
+    [].tap do |nearby|
+      x, y = position
+      NEIGHBORS.each do |(dx, dy)|
+        new_position = [x + dx, y + dy]
+        if new_position.all? { |pos| pos.between(0, board.grid_size - 1)}
+          nearby << Tile.new([new_x, new_y], board, self)
+        end
+      end
+    end
+  end
 
 end
