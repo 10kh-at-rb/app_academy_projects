ab0e25cb54953596a4981d2b9c2fb0f02d0a15f7
eric firth <ericfirth@gmail.com>
2015-04-02 10:03:22 -0400

    Add syntactic sugar to board

--- checkers.rb
+++ checkers.rb
@@ -1,16 +1,28 @@
 class Piece
-  def initialize(pos)
+  DELTAS = [
+    [1, 0],
+    [-1, 0]
+  ]
+
+  def initialize(color, board, pos)
     @king_piece = false
+    @pos, @board = board, pos
   end
 
-  def perform_slide
-
+  def perform_slide(from,to)
+    #moves the piece on the from the from spot to the to space
   end
 
-  def perform_jump
+  def perform_jump(from,to)
   end
 
   def move_diffs
+
+  end
+
+  def available_jump?
+    #checks to see if there is a piece at the delta & if the delta *2
+    #is empty
   end
 
   def legal_move?(from,to)
@@ -36,8 +48,23 @@ class Board
     @grid = grid
   end
 
+  def [](pos)
+    i,j = pos
+    @grid[i][j]
+  end
+
+  def []=(pos, piece)
+    i,j = pos
+    @grid[i][j] = piece
+  end
+
 end
 
 class Checkers
+
+  def initialize
+    @players = { white: HumanPlayer.new(:white),
+                black: HumanPlayer.new(:black) }
+  end
 
 end
