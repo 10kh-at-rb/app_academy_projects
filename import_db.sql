e4f950ffaa2d2aa5771e24f0c4ed7506dbee1561
rhoen+eric <rhoen+eric@appacademy.io>
2015-04-07 11:23:48 -0400

    Create tables in SQL

---
+++ import_db.sql
@@ -0,0 +1,55 @@
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  body TEXT,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  body TEXT,
+  parent_id INTEGER,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (parent_id) REFERENCES replies(id)
+);
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Rhoen', 'Pruesse-Adams'),
+  ('Eric', 'Firth');
+
+
+INSERT INTO
+  questions ()
