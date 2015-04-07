d0a420b6e126be2e475ccb90cc46afbe67a9ed92
rhoen+eric <rhoen+eric@appacademy.io>
2015-04-07 11:57:24 -0400

    add find by method and self.all class methods on user class

--- question_db.rb
+++ question_db.rb
@@ -26,3 +26,40 @@ class QuestionDatabase < SQLite3::Database
     self.type_translation = true
   end
 end
+
+class User
+
+  def self.all
+    results = QuestionDatabase.instance.execute('SELECT * FROM users')
+    results.map { |result| User.new(result) }
+  end
+
+  def self.find_by_id(id)
+    results = QuestionDatabase.instance.execute(<<-SQL, id)
+    
+    SELECT
+      *
+    FROM
+      users
+    WHERE
+      id = ?
+    SQL
+
+    User.new(results)
+  end
+
+  attr_reader :fname, :lname, :id
+
+  def initialize(options = {})
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
+  def name
+    "#{fname} #{lname}"
+  end
+
+
+
+end
