9938763bbd11bd90ab8e84ba49956f3109c86799
eric+liann <eric+liann@appacademy.io>
2015-04-09 15:41:40 -0400

    Create a Poll#results method.

--- app/models/question.rb
+++ app/models/question.rb
@@ -15,4 +15,20 @@ class Question < ActiveRecord::Base
   has_many :responses,
     through: :answer_choices,
     source: :responses
+
+  def results
+    choices = self
+    .answer_choices
+    .select("answer_choices.*, COUNT(responses.*) AS num_responses")
+    .joins("LEFT OUTER JOIN responses ON responses.answer_choice_id = answer_choices.id")
+    .where("answer_choices.question_id = ?", id)
+    .group("answer_choices.id")
+
+    results = {}
+    choices.each do |choice|
+      results[choice.text] = choice.num_responses
+    end
+    results
+
+  end
 end
