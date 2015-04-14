7d3c8d3bf9fcbc3322f22c4acc8f4bd330d95bfe
dylan+eric <dylan+eric@appacademy.io>
2015-04-14 14:08:00 -0400

    spell out link_to urls and refactor cat forms

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,7 +1,8 @@
 class Cat < ActiveRecord::Base
+  COLORS = %w( orange brown white black grey )
   validates :birth_date, :color, :name, :sex, presence: true
   validates_inclusion_of :sex, in: %w( m f t )
-  validates_inclusion_of :color, in: %w( orange brown white black grey )
+  validates_inclusion_of :color, in: COLORS
 
   def age
     (Time.now.utc.to_date.year - self.birth_date.year)
