c1770e490c6a9df81e9093e50bd0066188563c4f
Alex+Eric <Alex+Eric@aa.io>
2015-04-17 12:23:01 -0400

    Complete Phase I

--- app/controllers/concerns/sessions_controller.rb
+++ app/controllers/concerns/sessions_controller.rb
@@ -17,4 +17,10 @@ class SessionsController < ApplicationController
     render :new
   end
 
+  def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    redirect_to new_session_url
+  end
+
 end
