a3906d55400087b3234296e0240ff22f016d9d3b
harris+eric <harris+eric@appacademy.io>
2015-04-20 14:59:51 -0400

    Phase 1 complete with refactoring

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 require 'rails_helper'
 
+# spec heloer
+
+# dry out before each by bringing fillins up  a level
+
+
 feature "the signup process" do
 
   scenario "has a new user page" do
@@ -9,29 +14,20 @@ feature "the signup process" do
   end
 
     feature "signing up a user" do
-      before(:each) do
-        visit new_user_url
-        fill_in 'Username', with: 'testing_username'
-        fill_in 'Password', with: 'password'
-      end
-
-
       scenario "user can't sign-in in with a blank password" do
-        fill_in 'Password', with: ''
-        click_on "Create User"
+        sign_up('testing_username','')
         expect(page).to have_content "Password is too short"
         expect(page).to have_content "New user"
       end
 
       scenario "user can't sign-in with a blank username" do
-        fill_in 'Username', with: ''
-        click_on "Create User"
+        sign_up('','password')
         expect(page).to have_content "Username can't be blank"
         expect(page).to have_content "New user"
       end
 
       scenario "shows username on the homepage after signup"  do
-        click_on "Create User"
+        sign_up_as_tester
         expect(page).to have_content 'testing_username'
         expect(page.current_path).to_not eq new_user_url
       end
@@ -42,19 +38,8 @@ end
 
   feature "logging in" do
 
-      before(:each) do
-        visit new_user_url
-        fill_in 'Username', with: 'testing_username'
-        fill_in 'Password', with: 'password'
-        click_on 'Create User'
-        click_on 'Sign Out'
-        fill_in 'Username', with: 'testing_username'
-        fill_in 'Password', with: 'password'
-      end
-
     scenario "shows username on the homepage after login" do
-      # save_and_open_page
-      click_on 'Sign In'
+      sign_in_as_tester
       expect(page).to have_content 'testing_username'
       expect(page).to have_content 'Sign Out'
     end
@@ -70,12 +55,8 @@ end
     end
 
     scenario "doesn't show username on the homepage after logout" do
-      visit new_user_url
-      fill_in 'Username', with: 'testing_username'
-      fill_in 'Password', with: 'password'
-      click_on 'Create User'
+      sign_up_as_tester
       click_on "Sign Out"
-
       expect(page).to_not have_content "testing_username"
     end
 
