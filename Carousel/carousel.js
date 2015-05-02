3d5b4479306e7b0ad66331cc351f81cf73a5d398
App Academy Student <appacademy@Apps-Mac-mini-26.local>
2015-05-02 17:10:29 -0400

    Carousel fixed

--- Carousel/carousel.js
+++ Carousel/carousel.js
@@ -3,33 +3,31 @@
     this.$el = $(el);
     this.activeIdx = 0;
     this.numItems = $("li").length;
-    this.clickHandlers();
+
+    this.$el.on("click", "a.slide-left", this.slideLeft.bind(this));
+    this.$el.on("click", "a.slide-right", this.slideRight.bind(this));
+    // this.clickHandlers();
   }
 
-  $.Carousel.prototype.clickHandlers = function() {
-    that = this;
-    var oldActiveMoveDir
-    this.$el.on("click", "a", function(event) {
-      var $oldActive = $("li").eq(that.activeIdx);
-      if (event.currentTarget.className === "slide-left") {
-        $oldActive.addClass("right");
-        that.slide(1);
-        console.log("old picture index", $oldActive.index())
-      } else {
-        $oldActive.addClass("left");
-        that.slide(-1);
-        console.log("old picture index", $oldActive.index())
-      }
-      console.log(that.activeIdx)
-
-      $oldActive.on("transitionend", function() {
-        $oldActive.removeClass("active")
-          .removeClass("right")
-          .removeClass("left")
-          // .removeClass(oldActiveMoveDir);
-      })
-    })
-  };
+  // $.Carousel.prototype.clickHandlers = function() {
+  //   that = this;
+  //   var oldActiveMoveDir
+  //   this.$el.on("click", "a", function(event) {
+  //     var $oldActive = $("li").eq(that.activeIdx);
+  //     if (event.currentTarget.className === "slide-left") {
+  //       $oldActive.addClass("right");
+  //       that.slideLeft;
+  //       console.log("old picture index", $oldActive.index())
+  //     } else {
+  //       $
+  //       that.slideRight;
+  //       console.log("old picture index", $oldActive.index())
+  //     }
+  //     console.log(that.activeIdx)
+  //
+  //
+  //   })
+  // };
 
   $.Carousel.prototype.slideLeft = function() {
     this.slide(1);
@@ -40,29 +38,26 @@
   }
 
   $.Carousel.prototype.slide = function(num) {
-    this.activeIdx += num;
-    this.activeIdx = this.activeIdx % this.numItems;
-    if (this.activeIdx < 0) {
-      this.activeIdx += this.numItems;
-    }
-
+    var $oldActive = $("li").eq(this.activeIdx);
+    this.activeIdx = (this.activeIdx += num + this.numItems) % this.numItems;
 
     if (num === 1) {
-      $newPic = $("li").eq(this.activeIdx)
-        .addClass("active")
-        .addClass("left");
-        console.log("new picture index", $newPic.index())
-
+      $newPic = $("li").eq(this.activeIdx).addClass("active left");
+      $oldActive.addClass("right");
     } else {
-      $newPic = $("li").eq(this.activeIdx)
-        .addClass("active")
-        .addClass("right");
-        console.log("new picture index", $newPic.index())
+      $newPic = $("li").eq(this.activeIdx).addClass("active right")
+      $oldActive.addClass("left");
     }
 
     setTimeout(function() {
       $newPic.removeClass("left").removeClass("right");
     }, 0);
+
+    $oldActive.one("transitionend", function() {
+      $oldActive.removeClass("active")
+        .removeClass("right")
+        .removeClass("left");
+    })
   }
 
   $.fn.carousel = function () {
