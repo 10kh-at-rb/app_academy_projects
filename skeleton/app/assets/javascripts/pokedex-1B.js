743d82db60e68a3dcb02ca02183fd337d34e1cc0
nathan+eric <nathan+eric@aa.io>
2015-05-05 11:59:38 -0400

    Finish Phase 4: created templates for all actions from yesterday

--- skeleton/app/assets/javascripts/pokedex-1B.js
+++ skeleton/app/assets/javascripts/pokedex-1B.js
@@ -1,30 +1,14 @@
 Pokedex.RootView.prototype.renderPokemonDetail = function (pokemon) {
-  var $detail = $('<div class="detail">');
-  // Show the image
-  $detail.append('<img src="' + pokemon.escape('image_url') + '"><br>');
-  // Show the attributes
-  for (var attr in pokemon.attributes) {
-    if (pokemon.get(attr) && attr !== 'id' && attr !== 'image_url') {
-      $detail.append(
-        '<span style="font-weight:bold;">' + attr + ':</span> ' +
-          pokemon.get(attr) + '<br>'
-      );
-    }
-  }
-  this.$pokeDetail.html($detail);
-
-  // Phase 2C.
-  this.$pokeDetail.append(
-    '<span style="font-weight: bold;">Toys:</span><br>'
-  );
-  var $toys = $('<ul class="toys"></ul>');
-  this.$pokeDetail.append($toys);
-
-  pokemon.fetch({
-    success: (function() {
-      this.renderToysList(pokemon.toys());
-    }).bind(this)
+  this.$pokeDetail.empty();
+  var content = JST['pokemonDetail']({ pokemon: pokemon });
+  // debugger;
+  pokemon.fetch( {
+    success: function(){
+      pokemon.toys().forEach(this.addToyToList.bind(this))
+    }.bind(this)
   });
+  this.$pokeDetail.append(content)
+
 };
 
 Pokedex.RootView.prototype.selectPokemonFromList = function (event) {
