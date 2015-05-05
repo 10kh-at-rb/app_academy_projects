95cba6858f7bd3fb052b72d25de0193a014f4ede
nathan+eric <nathan+eric@aa.io>
2015-05-05 14:39:46 -0400

    Phase 5 complete: Pokemon views

--- skeleton/app/assets/javascripts/pokedex-5.js
+++ skeleton/app/assets/javascripts/pokedex-5.js
@@ -2,6 +2,7 @@ Pokedex.Views = {}
 
 Pokedex.Views.PokemonIndex = Backbone.View.extend({
   events: {
+    "click li": "selectPokemonFromList"
   },
 
   initialize: function () {
@@ -27,25 +28,72 @@ Pokedex.Views.PokemonIndex = Backbone.View.extend({
   },
 
   selectPokemonFromList: function (event) {
+    var $target = $(event.currentTarget);
+
+    var pokeId = $target.data('id');
+    var pokemon = this.pokes.get(pokeId);
+
+    var view = new Pokedex.Views.PokemonDetail({model: pokemon});
+    view.refreshPokemon();
+
   }
 });
 
 Pokedex.Views.PokemonDetail = Backbone.View.extend({
   events: {
+    "click .toys li": "selectToyFromList"
+  },
+
+  initialize: function (options) {
+    this.model = options["model"];
   },
 
   refreshPokemon: function (options) {
+    this.model.fetch({
+      success: function() {
+        $("#pokedex .pokemon-detail").html(this.$el);
+        this.render();
+        this.model.toys().forEach( function (toy) {
+          var content = JST["toyListItem"]({ toy: toy });
+          var $toysUl = $("ul.toys");
+          $toysUl.append(content);
+        })
+      }.bind(this)
+    })
   },
 
   render: function () {
+    this.$el.empty();
+    var content = JST['pokemonDetail']({ pokemon: this.model });
+    this.$el.append(content);
+    // pokemon.fetch( {
+    //   success: function(){
+    //     pokemon.toys().forEach(this.addToyToList.bind(this))
+    //   }.bind(this)
+    // });
+
   },
 
   selectToyFromList: function (event) {
+    var $currentTarget = $(event.currentTarget)
+    var toy = this.model.toys().get($currentTarget.data("id"))
+    var toyDetailView = new Pokedex.Views.ToyDetail({toy: toy, pokes: []})
+    $("#pokedex .toy-detail").html(toyDetailView.$el);
+
+    toyDetailView.render();
   }
 });
 
 Pokedex.Views.ToyDetail = Backbone.View.extend({
-  render: function () {
+  render: function (options) {
+    console.log("hi")
+    var content = JST["toyDetail"]({ toy: this.toyModel, pokes: this.pokes});
+    this.$el.html(content);
+  },
+
+  initialize: function (options) {
+    this.toyModel = options["toy"];
+    this.pokes = options["pokes"];
   }
 });
 
