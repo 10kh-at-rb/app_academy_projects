fa3b4b8eafd306ec624011ee8e5b5ad10d7c7bce
eric+thomas <eric+thomas@aa.io>
2015-05-06 10:41:15 -0400

    Phase 1: Create post basics in rails, beginning views in backbone

---
+++ Journal/app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render :json
+    else
+      render json: @post.errors.full_messages, status: 422
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
