c141bbf58d9cef4d7fa2c063316d98195a4d82d7
eric+eric <eric+eric@aa.io>
2015-05-07 11:35:33 -0400

    initial commit + phase zero working. feed#latest_entries'

---
+++ skeleton/app/assets/javascripts/news_reader.js
@@ -0,0 +1,13 @@
+window.NewsReader = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello Erics! I am your backbone.');
+  }
+};
+
+$(document).ready(function(){
+  NewsReader.initialize();
+});
